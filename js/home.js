e8cc61e6602d3f34aa901ab4f8b324b2a3386d4d
HoangTrung <31776998+lhtrung307@users.noreply.github.com>
2018-08-17 12:59:40 +0700

    Add files via upload
    Init web page

---
+++ js/home.js
@@ -0,0 +1,47 @@
+// let MYNAME = Symbol('myname');
+// class Car{
+// 	constructor(make, model){
+// 		this.make = make;
+// 		this.model = model;
+// 	}
+// 	[MYNAME](){
+// 		return {
+// 			make: this.make,
+// 			model: this.model
+// 		};
+// 	}
+// 	["add"](a, b){
+// 		return a + b;
+// 	}
+
+// 	[1 + 2](){
+// 		return "three";
+// 	}
+// }
+// let MazdaMPV = new Car("Mazda", "MPV");
+// console.log(MazdaMPV.add(4, 5));
+// console.log(MazdaMPV[3]());
+// console.log(MazdaMPV[MYNAME]());
+// 
+
+let CAT = Symbol('cat');
+class features{
+	constructor(name, age){
+		this.name = name;
+		this.age = age;
+	}
+	[CAT](){
+		return {
+			name: this.name,
+			age: this.age
+		};
+	}
+	["eat"](a, b){
+		return a + b;
+	}
+}
+let Tom = new features("Tom", 2);
+console.log(Tom[CAT]());
+console.log(Tom.eat("mouse", "rice"));
+
+
