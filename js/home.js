76095d650c077624b6471fd095c13fe307c198f0
Hoang Trung <lhtrung307@gmail.com>
2018-08-17 14:51:36 +0700

    Update home.js

--- js/home.js
+++ js/home.js
@@ -24,24 +24,26 @@
 // console.log(MazdaMPV[MYNAME]());
 // 
 
-let CAT = Symbol('cat');
-class features{
-	constructor(name, age){
-		this.name = name;
-		this.age = age;
-	}
-	[CAT](){
-		return {
-			name: this.name,
-			age: this.age
-		};
-	}
-	["eat"](a, b){
-		return a + b;
-	}
-}
-let Tom = new features("Tom", 2);
-console.log(Tom[CAT]());
-console.log(Tom.eat("mouse", "rice"));
+// let CAT = Symbol('cat');
+// class features{
+// 	constructor(name, age){
+// 		this.name = name;
+// 		this.age = age;
+// 	}
+// 	[CAT](){
+// 		return {
+// 			name: this.name,
+// 			age: this.age
+// 		};
+// 	}
+// 	["eat"](a, b){
+// 		return a + b;
+// 	}
+// }
+// let Tom = new features("Tom", 2);
+// console.log(Tom[CAT]());
+// console.log(Tom.eat("mouse", "rice"));
+
+window.alert("Okay");
 
 
