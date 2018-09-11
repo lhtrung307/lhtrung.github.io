30bc45fe206940582025103d9a9622b18153066c
Phạm Thị Thuỳ Trang <thuytrang.bcvt2015@gmail.com>
2018-09-12 02:51:06 +0700

    big update

--- js/home.js
+++ js/home.js
@@ -1,3 +1,25 @@
+
+// slide
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+	var i;
+	var slides = document.getElementsByClassName("mySlides");
+	var dots = document.getElementsByClassName("dot");
+	for (i = 0; i < slides.length; i++) {
+		slides[i].style.display = "none";
+	}
+	slideIndex++;
+	if (slideIndex > slides.length) { slideIndex = 1 }
+	for (i = 0; i < dots.length; i++) {
+		dots[i].className = dots[i].className.replace(" active", "");
+	}
+	slides[slideIndex - 1].style.display = "block";
+	dots[slideIndex - 1].className += " active";
+	setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
+
 const recentPosts = document.querySelector(".recentItems");
 
 function renderRecentPost(doc) {
@@ -25,7 +47,7 @@ getRecentText();
 
 const posts = document.querySelector("div#posts");
 
-function createPostImg(){
+function createPostImg() {
 	let img = document.createElement('img');
 	let imgSrc = "https://goo.gl/uTbQiH"
 	img.src = imgSrc;
@@ -33,7 +55,7 @@ function createPostImg(){
 	return img;
 }
 
-function createPostTitle(title){
+function createPostTitle(title) {
 	let postTitle = document.createElement('a');
 	postTitle.id = "postTitle";
 	postTitle.className = "title";
@@ -43,14 +65,14 @@ function createPostTitle(title){
 	return postTitle;
 }
 
-function createAuthorLink(author, href){
+function createAuthorLink(author, href) {
 	let authorLink = document.createElement('a');
 	authorLink.href = href;
 	authorLink.textContent = author;
 	return authorLink;
 }
 
-function createPostDayElement(createDay){
+function createPostDayElement(createDay) {
 	let postDay = document.createElement("span");
 	postDay.id = "postDay";
 	postDay.className = "time";
@@ -64,12 +86,12 @@ function renderPost(doc) {
 
 	let divContainer = document.createElement('div');
 	divContainer.className = "item col-lg-12 col-md-12 col-sm-12 col-xs-12";
-	
+
 	let img = createPostImg();
-	
+
 	let divContainerContent = document.createElement("div");
 	divContainerContent.className = "item-information col-lg-8 col-md-8 col-sm-8 col-xs-8";
-	
+
 	let postTitle = createPostTitle(title);
 
 	let general = document.createElement('div');
@@ -80,7 +102,7 @@ function renderPost(doc) {
 	let authorLink = createAuthorLink(author, "https://goo.gl/uTbQiH");
 	authorSpan.appendChild(authorLink);
 	let postDay = createPostDayElement(createDay);
-	
+
 	let comment = document.createElement('span');
 	comment.class = "comment";
 
@@ -96,29 +118,45 @@ function renderPost(doc) {
 	let postContent = document.createElement('div');
 	postContent.id = "postContent";
 	postContent.className = "content";
-	
+
 	let readmore = document.createElement("span");
 	readmore.className = "readmore";
 	let readmoreLink = document.createElement('a');
 	readmoreLink.href = "#";
 	readmoreLink.textContent = "READ MORE";
 	readmore.appendChild(readmoreLink);
-	
+
 	postContent.appendChild(readmore);
 
 	divContainerContent.appendChild(postTitle);
 	divContainerContent.appendChild(general);
 	divContainerContent.appendChild(postContent);
-	
+
 	divContainer.appendChild(img);
 	divContainer.appendChild(divContainerContent);
 
 	posts.appendChild(divContainer);
 }
-		
+
 db.collection('Posts').get().then((snapshot) => {
 	snapshot.docs.forEach(doc => {
 		renderPost(doc);
 	})
 })
-		
+
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("myBtn").style.display = "block";
+    } else {
+        document.getElementById("myBtn").style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
